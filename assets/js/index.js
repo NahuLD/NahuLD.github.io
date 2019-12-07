5c66a82ad8a6d2383f614a405df10b2f81c717c1
Nahuel <nahueldolores@hotmail.com>
2019-12-07 12:13:03 -0300

    Made a couple of changes

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function copyToClipboard(text) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(text).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
